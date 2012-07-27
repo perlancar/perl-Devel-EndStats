c0cfc437157caa7c6e971942d881e908cc2ba577
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2012-07-27 10:15:37 +0700

    ux: By default display program runtime in secs, but module load time in ms

--- lib/Devel/EndStats.pm
+++ lib/Devel/EndStats.pm
@@ -125,7 +125,7 @@ END {
 
     if ($begin_success) {
 
-        $stats .= sprintf "# Program runtime duration: %.6fs\n", $secs;
+        $stats .= sprintf "# Program runtime duration: %.3fs\n", $secs;
 
         my $files = 0;
         my $lines = 0;
@@ -165,8 +165,8 @@ END {
             for my $r (sort $sortsub keys %inc_info) {
                 next unless $inc_info{$r}{lines};
                 $inc_info{$r}{time} ||= 0;
-                $stats .= sprintf "#   #%3d  %5d lines  %.6fs(%3d%%)  %s (loaded by %s)\n",
-                     $inc_info{$r}{order}, $inc_info{$r}{lines}, $inc_info{$r}{time}, $secs ? $inc_info{$r}{time}/$secs*100 : 0,
+                $stats .= sprintf "#   #%3d  %5d lines  %7.3fms(%3d%%)  %s (loaded by %s)\n",
+                     $inc_info{$r}{order}, $inc_info{$r}{lines}, $inc_info{$r}{time}*1000, $secs ? $inc_info{$r}{time}/$secs*100 : 0,
                          $r, $inc_info{$r}{caller};
             }
         }
